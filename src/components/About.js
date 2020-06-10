b416ae8b41f5bbcb69d66ed7d6a5e4150e3d5471
hamidval <hamid-val@hotmail.co.uk>
2020-06-10 02:12:40 +0100

    added service grid

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
 
    constructor(props){
         super(props)
-        this.state={}
+        this.state={services:["websites","mobile apps","seo","web apps","websites","mobile apps","seo","web apps"]}
 
    }
 
@@ -19,22 +19,34 @@ class About extends Component {
         
         <div className='par-div'>
 
-            <div className='about-div-40'>
+            <div >
                 <div className='abs-txt strokeme'>About Us</div>
-                <img className='about-img' src={londonSky}/>
+                <img className='img-fluid' alt='Responsive image' src={londonSky}/>
             
             </div>
-            <div className='about-div'>
-                <div>Who we are? What we do?</div>
+            <div className='about-div div-margin'>
+                <div className='h3'>Who we are? What we do?</div>
                 <div>
-                    <p>
+                    <p className='h6'>
                         We are a small development group based in London, looking to take on small and medium sized projects
                         Our developers are young, ambitious and skilled in web and app development projects
                     </p>
                 </div>
             </div>
-            <div className='about-div'>
-                <div>Services</div>
+            <div className='about-div div-margin'>
+                <div className='h3'>Services</div>
+                <div className='h6 grid'>
+                    {
+                        this.state.services.map((item,index)=>(
+                            <div className='service'>
+                               <h6>{item}</h6>
+                            </div>
+
+                        ))
+                    }
+                
+                </div>
+
                 <div>
 
                 </div>
