01207eee703429f0625dae9346c9adc2b54e3889
hamidval <hamid-val@hotmail.co.uk>
2020-06-22 13:31:54 +0100

    added basename

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     return(
     <div className="App" >
-   <Router>
+   <Router basename={'dev-website'}>
     <div >
       
       <Route exact path='/' render={() => (<Home  />)}/> 
